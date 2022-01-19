f3784d7ae9bda64edd4d912b2c237b12fffa0ed7
Ziyi Zhang <ziyizhang@MacBook-Pro-2.local>
2022-01-19 13:05:05 -0500

    new

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, request, render_template
+from http.client import responses
+from typing import final
+from flask import Flask, request, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import surveys, satisfaction_survey, personality_quiz
+
+RESPONSES = "responses"
+
 
 app = Flask(__name__)
 app.debug=True
 app.config['SECRET_KEY'] = "key"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 
 debug = DebugToolbarExtension(app)
 
@@ -12,25 +19,52 @@ debug = DebugToolbarExtension(app)
 def home_page():
     
     satisfaction= satisfaction_survey
-    
+        
     return render_template('homepage.html',satisfaction=satisfaction)
 
 
+@app.route('/begin')
+def begin_page():
+    
+    session[RESPONSES]=[]
 
-@app.route('/questions')
-def questions_page():
+
+    return redirect('/questions/0')
+
+@app.route('/questions/<int:questionid>')
+def questions_page(questionid):
+    responses = session.get(RESPONSES)
+
     satisfaction= satisfaction_survey
     
-    return render_template('questions.html',satisfaction=satisfaction)
+    if (responses is None):
+        return redirect("/")
+
+    if (len(responses) != questionid):
+        flash(f"Invalid question id: {questionid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    
+    return render_template('questions.html',question_num=questionid, satisfaction=satisfaction)
+    
     
 
 
 @app.route('/result', methods=['POST'])
 def survey_result():
-    user_result=[]
+
+    
     answer= request.form['result']
-    user_result.append(answer)
-    return render_template('result.html',answer=user_result)
+    final_result=session[RESPONSES]
+    final_result.append(answer)
+    session[RESPONSES]=final_result    
+    
+    if len(final_result) == len(satisfaction_survey.questions):
+        return render_template('result.html',answer=final_result)
+    else:
+        return redirect(f'/questions/{len(final_result)}')
+    
+
 
 @app.route('/thankyou',methods=['POST'])
 def thankyou_page():
